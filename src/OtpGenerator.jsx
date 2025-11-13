c60b57dc9f6703d647624b50ef7ec579097db304
baxaw <baxDjo@outlook.fr>
2025-11-13 13:13:03 -0500

    first commit for the app generator password once

---
+++ src/OtpGenerator.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const OtpGenerator = () => {
+  return (
+    <div className='container'>
+        <h1> OTP Generator</h1>
+        <p>Click on the Button to get code</p>
+        <button>Generate OTP</button>
+    </div>
+  )
+}
+
+export default OtpGenerator
