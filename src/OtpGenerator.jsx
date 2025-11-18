8e7e9df07ce2122012cb536778a39013439724cc
baxaw <baxDjo@outlook.fr>
2025-11-17 22:53:18 -0500

    Application fonctionnelle, ajout du style

--- src/OtpGenerator.jsx
+++ src/OtpGenerator.jsx
@@ -1,13 +1,62 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+const OTP_DURATION = 10;
+/**
+ * Description placeholder
+ *
+ * @returns {*} 
+ */
 const OtpGenerator = () => {
+
+  const [count, setCount] = useState(5);
+  const [codeOTP, setCodeOTP] = useState(null);
+
+  useEffect(() => {
+    if (count === 0) {
+      // reset automatique
+      return;
+    }
+    const timer = setTimeout(() => {
+      setCount((count) => count - 1);
+    }, 1000);
+
+    if (count === 0 && codeOTP !== null) {
+      setCodeOTP(null); // OTP expire
+    }
+    return () => clearTimeout(timer);
+  }
+    , [count, codeOTP]);
+
+  const generateOTP = () => {
+    const newOtp = Math.floor(100000 + Math.random() * 900000).toString();
+    setCodeOTP(newOtp);
+    setCount(5); // 10-second validity
+  };
+
+  const gaugeProgress =
+  codeOTP && count > 0 ? (count / OTP_DURATION) * 100 : 0;
+
   return (
-    <div className='container'>
-        <h1> OTP Generator</h1>
-        <p>Click on the Button to get code</p>
-        <button>Generate OTP</button>
+    <div className='min-h-screen bg-gray-900 text-gray-200 flex flex-col items-center justify-center gap-6 p-6'>
+      <h1 className='text-2xl font-bold text-white uppercase tracking-wide' id='otp-title'> One Time Password Generator</h1>
+
+      {codeOTP === null ? (<h2>"Click 'Generate OTP' to get a code"</h2>)
+        : (<h2>{codeOTP}</h2>)
+      }
+      <p className="text-md text-gray-300 font-medium mt-2" id='otp-timer' aria-live="polite">
+        {codeOTP && count > 0 && `Expires in: ${count} seconds`}
+        {!codeOTP !== null && count === 0 &&
+          "OTP expired. Click the button to generate a new OTP."}
+      </p>
+      <CircularProgress variant="determinate" value={gaugeProgress
+    
+      } />
+
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" id='generate-otp-button' onClick={generateOTP}
+      >
+        Generate OTP</button>
     </div>
   )
 }
 
-export default OtpGenerator
+export default OtpGenerator;
